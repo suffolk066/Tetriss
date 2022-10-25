64fb8a48c18715b03a80a0da5192354ba03aaf6b
KH302 <KH302@DESKTOP-QFUMFDO>
2022-10-25 14:31:02 +0900

    패널 작성

---
+++ Tetriss/src/Tetriss/MyRect.java
@@ -0,0 +1,33 @@
+package Tetriss;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class MyRect {
+	int x, y, w, h;
+	Color c;
+	
+	public MyRect(int x, int y, int w, int h, Color c) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.c = c;
+		
+		
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(c);
+		g.fillRect(x, y, w, h);
+	}
+	
+	public void draw2(Graphics g) {
+		g.setColor(c);
+		g.fillRect(x, y, w, h);
+	}
+	
+	public void setC(Color c) {
+		this.c = c;
+	}
+}
--- Tetriss/src/Tetriss/mainFrame.java
+++ Tetriss/src/Tetriss/mainFrame.java
@@ -1,19 +1,31 @@
 package Tetriss;
 
-import java.awt.Container;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class mainFrame extends JFrame {
 	Container con = getContentPane();
+	MyPanel panel = new MyPanel();
 	
 	public mainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("서버");
-		setSize(300, 300);
+		setTitle("메인 프레임");
+		setSize(528, 650);
+		setFrame();
 		setVisible(true);
 	}
+
+	private void setFrame() {
+//		JPanel pnlFrame = new JPanel();
+//		con.add(pnlFrame);
+		con.add(panel);
+		panel.setFocusable(true);
+		panel.requestFocus();
+		
+	}
+
 
 	public static void main(String[] args) {
 		new mainFrame();
