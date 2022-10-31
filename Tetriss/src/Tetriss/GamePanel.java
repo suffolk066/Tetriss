77a403fb0a071d0a5e6b7dbcc56437285824ae20
KH302 <KH302@DESKTOP-QFUMFDO>
2022-10-31 17:15:28 +0900

    Refactor

--- Tetriss/src/Tetriss/GamePanel.java
+++ Tetriss/src/Tetriss/GamePanel.java
@@ -2,101 +2,39 @@ package Tetriss;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel {
-	final int BOARD_ROWS = 15;
-	final int BOARD_COLS = 10;
-	final int GRID = 30;
-	final int DISTANCE = 31;
-	int[][] gameBoard = new int[BOARD_ROWS][BOARD_COLS];
 	
-	GenerateRect rects[][];
-	Listener keyListener = new Listener();
-	
-	// ㅗ자 모양 도형
-	int rectShape[][] = {
-					{2, 5},
-					{3, 4},
-					{3, 5},
-					{3, 6}
-					};
-	
+	private final int BOARD_ROWS = 15;
+	private final int BOARD_COLS = 10;
+	private final int CELL_SIZE = 30;
+	int[][] cell;
 	
 	public GamePanel() {
-		rects = new GenerateRect[BOARD_ROWS][BOARD_COLS];
-		for (int i = 0; i < BOARD_ROWS; i++) {
-			for (int j = 0; j < BOARD_COLS; j++) {
-//				if(i == 0 || i == BOARD_ROWS-1 || j == 0 || j == BOARD_COLS-1) {
-//					rects[i][j] = new GenerateRect(j*DISTANCE, i*DISTANCE, GRID, GRID, Color.BLACK);
-//				} else {
-					rects[i][j] = new GenerateRect(j*DISTANCE, i*DISTANCE, GRID, GRID, Color.LIGHT_GRAY);
-//				}
-			}
-		}
-		addKeyListener(keyListener);
-	} // panel
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		// 게임판 그리기
-		for (int i = 0; i < BOARD_ROWS; i++) {
-			for (int j = 0; j < BOARD_COLS; j++) {
-				rects[i][j].drawRect(g);
-			}
-		}
-		// ㅗ 자 모양 블록 그리기
-		for (int i = 0; i < 4; i++) {
-			rects[rectShape[i][0]] [rectShape[i][1]].setColor(Color.RED);
-		}
+		setBounds(100, 100, 301, 451);
+		setBackground(Color.white);
 	}
 	
-	class Listener extends KeyAdapter {
-
-		@Override
-		public void keyPressed(KeyEvent e) {
-			int key = e.getKeyCode();
-			System.out.println("input:" + key);
-			
-			switch (key) {
-			case KeyEvent.VK_LEFT:
-				for (int i = 0; i < 4; i++) {
-					if (rectShape[i][1] < 1) {
-						System.out.println("왼쪽 이동 불가");
-						return;
-					}
-					// 원래 색 지우기
-					rects[rectShape[i][0]] [rectShape[i][1]].setColor(Color.LIGHT_GRAY);
-					rectShape[i][1] -= 1;
-					// 이동한 칸에 다시 입히기
-					rects[rectShape[i][0]] [rectShape[i][1]].setColor(Color.RED);
-				}
-				break;
-			case KeyEvent.VK_RIGHT:
-				for (int i = 0; i < 4; i++) {
-					if (rectShape[i][1] > 8 ) {
-						System.out.println("오른쪽 이동 불가");
-						return;
-					}
-					// 원래 색 지우기
-					System.out.println("변경전: " + rectShape[i][1]);
-					rects[rectShape[i][0]] [rectShape[i][1]].setColor(Color.LIGHT_GRAY);
-					System.out.println(rects[rectShape[i][0]] [rectShape[i][1]].getColor());
-					rectShape[i][1] += 1;
-					System.out.println("변경후: " + rectShape[i][1]);
-					// 이동한 칸에 다시 입히기
-					rects[rectShape[i][0]] [rectShape[i][1]].setColor(Color.RED);
-					System.out.println(rects[rectShape[i][0]] [rectShape[i][1]].getColor());
-				}
-				break;
-			}
-			repaint();
-		} // pressed
+	public void block() {
 		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		for (int i = 0; i < BOARD_COLS; i++ ) {
+			g.drawRect(i * CELL_SIZE, 0, CELL_SIZE, CELL_SIZE);
+			for (int j = 0; j < BOARD_ROWS; j++) {
+				g.drawRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+			}
+		}
+		cell = new int[][]{{1, 0}, {1, 0}, {1, 0}, {1, 1}};
+		g.setColor(Color.red);
+		for (int i = 0; i < 4; i++) {
+			g.fillRect(0, i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+		}
 	}
 }
--- Tetriss/src/Tetriss/MainFrame.java
+++ Tetriss/src/Tetriss/MainFrame.java
@@ -8,25 +8,20 @@ import javax.swing.*;
 public class MainFrame extends JFrame {
 	Container con = getContentPane();
 	GamePanel panel = new GamePanel();
-	JLabel statusBar = new JLabel("0");
 	
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("테트리스 게임");
-		setSize(330, 520);
+		setSize(800, 800);
 		setFrame();
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
 	private void setFrame() {
+		con.setLayout(null);
 		con.add(panel);
-		panel.setFocusable(true);
-		panel.requestFocus();
-		con.add(statusBar, BorderLayout.SOUTH);
-		
 	}
-
 
 	public static void main(String[] args) {
 		new MainFrame();
